efca87e128f0570bfddd808b15d5e8b2fa8c1e75
rishabh <rishabh6401@gmail.com>
2024-10-13 19:30:43 +0530

    update

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
 const authRouter = require("./routes/auth/auth-routes");
 const adminProductsRouter = require("./routes/admin/products-routes");
 const adminOrderRouter = require("./routes/admin/order-routes");
@@ -15,50 +14,32 @@ const adminBrandRouter = require("./routes/admin/brands-route");
 const adminCategoryRouter = require("./routes/admin/categories-route");
 const shopSearchRouter = require("./routes/shop/search-routes");
 const shopReviewRouter = require("./routes/shop/review-routes");
-const shopRouter = require("./routes/admin/shops-route");
-
-
+const path = require("path");
 const commonFeatureRouter = require("./routes/common/feature-routes");
+const shopRouter = require("./routes/admin/shops-route");
 
 //create a database connection -> u can also
 //create a separate file for this and then import/use that file here
 
 mongoose
-  .connect("mongodb+srv://rishabh1234:EW5ibRCAbCDIt2vk@cluster0.uejtnl9.mongodb.net/NewEcommerce_app")
+  .connect("mongodb+srv://gozoomtechnologies:Gozoom%40Shashwat123@cluster1.ezuc0.mongodb.net/ShaswatEnterprises")
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.log(error));
 
 const app = express();
 const PORT = 5000;
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "DELETE", "PUT"],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Cache-Control",
-      "Expires",
-      "Pragma",
-    ],
-    credentials: true,
-  })
-);
 
-app.get("/",(req,res) => {
-  res.send("Backend is live")
-})
 
 app.use(cookieParser());
 app.use(express.json());
+
 app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
 app.use("/api/admin/orders", adminOrderRouter);
 app.use("/api/admin/beats", adminBeatRouter);
 app.use("/api/admin/brand", adminBrandRouter);
 app.use("/api/admin/category", adminCategoryRouter);
-app.use("/api/admin/users", shopRouter); 
 app.use("/api/salesman", salesmanRouter); // Use salesman routes
 
 app.use("/api/shop/products", shopProductsRouter);
@@ -66,8 +47,20 @@ app.use("/api/shop/cart", shopCartRouter);
 app.use("/api/shop/address", shopAddressRouter);
 app.use("/api/shop/order", shopOrderRouter);
 app.use("/api/shop/search", shopSearchRouter);
+app.use("/api/admin/users", shopRouter); 
 app.use("/api/shop/review", shopReviewRouter);
 
 app.use("/api/common/feature", commonFeatureRouter);
+
+app.use(express.static("public"))
+app.use("/*",(req,res)=>{
+  if (!req.originalUrl.startsWith("/api")) {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+  } else {
+    res.status(404).json({ error: "API route not found" });
+  }
+})
+
+
 
 app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
